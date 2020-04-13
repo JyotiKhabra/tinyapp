bc06dc9aca7f3205e08a0efede6422375db01066
vagrant <j_khabra11@hotmail.com>
2020-04-13 22:35:26 +0000

    add /hello page

---
+++ express_server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthoouselabs.ca",
+  "9sm5xk": "http://www.google.com"
+};
+
+app.get("/", (req, res) => 
+  res.send("Hello!");
+});
+app.get("/urls.json",(req, res) => {
+  res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port${PORT}!`);
+});
