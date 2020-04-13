1619b11eabcefe0c6d6d98f15dd3574ff8ea5ce0
vagrant <j_khabra11@hotmail.com>
2020-04-13 23:29:42 +0000

    Urls_index.ejs template

--- express_server.js
+++ express_server.js
@@ -10,15 +10,18 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.render("Hello!");
 });
 app.get("/urls.json",(req, res) => {
   res.json(urlDatabase);
 });
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.render("<html><body>Hello <b>World</b></body></html>\n");
 });
-
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase };
+  res.render("urls_index", templateVars);
+})
 app.listen(PORT, () => {
   console.log(`Example app listening on port${PORT}!`);
 });
