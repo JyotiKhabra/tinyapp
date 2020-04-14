682093b76c144fb365ee04286597616c791895ae
vagrant <j_khabra11@hotmail.com>
2020-04-14 00:18:39 +0000

    head partial header

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthoouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
 };
 
@@ -23,7 +23,8 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = {shortURL: req.params.shortURL, longURL: req.params.longURL};
+  const shortURL = req.params.shortURL;
+  let templateVars = {shortURL, longURL: urlDatabase[shortURL]};
   res.render("urls_show", templateVars);
 });
 app.listen(PORT, () => {
