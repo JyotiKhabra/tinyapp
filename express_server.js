a8b4fa994e6cd6d32e3d620ef5dfa9f4913e513e
vagrant <j_khabra11@hotmail.com>
2020-04-14 01:01:06 +0000

    added random number generator function

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -22,11 +24,23 @@ app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   let templateVars = {shortURL, longURL: urlDatabase[shortURL]};
   res.render("urls_show", templateVars);
 });
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.sen("Ok");
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port${PORT}!`);
 });
+
+function generateRandomString() {
+  let tinyURL = Math.random().stoString(36).substring(6);
+console.log("tinyUrl:", tinyURL);
+};
