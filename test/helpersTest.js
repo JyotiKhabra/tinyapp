5fc06c7cd2209423f1dbf0d75e33896e577d3ada
vagrant <j_khabra11@hotmail.com>
2020-04-17 04:58:48 +0000

    Linted File

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,17 +1,16 @@
 const { assert } = require('chai');
 
-const { getUserByEmail, urlsForUser } = require('../helper_functions/finduserhelpers')
-//const { getUserByEmail, urlsForUser } = require('./helper_functions/finduserhelpers.js')
+const { getUserByEmail, urlsForUser } = require('../helper_functions/finduserhelpers');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
@@ -24,24 +23,23 @@ const testUrlDatabase = {
 describe('urlsForUser', function() {
   it('should return urls associated with user', function() {
     const userID = urlsForUser("aJ481W", testUrlDatabase);
-    const expectedOutput = 
+    const expectedOutput =
     {"b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "aJ481W" },
-    "9sm5xk": { longURL: "http://www.google.ca", userID: "aJ481W" }}
+      "9sm5xk": { longURL: "http://www.google.ca", userID: "aJ481W" }};
     assert.deepEqual(userID, expectedOutput);
-  })
-})
+  });
+});
+
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user =  getUserByEmail("user@example.com", testUsers)
-    const expectedOutput =   
-   { id: "userRandomID", 
-      email: "user@example.com", 
-      password: "purple-monkey-dinosaur" };
+    const user =  getUserByEmail("user@example.com", testUsers);
+    const expectedOutput =
+   { id: "userRandomID",
+     email: "user@example.com",
+     password: "purple-monkey-dinosaur" };
 
     assert.deepEqual(user, expectedOutput);
   });
 });
 
-// const result = isPalindrome(word);
-// assert.isTrue(result);
